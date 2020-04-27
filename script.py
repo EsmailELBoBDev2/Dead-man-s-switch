5356e5d1c85e46c1f41c045e43cd6d64ddacf83b
Esmail EL BoB <esmailelbob@protonmail.com>
2020-04-27 11:36:34 +0200

    UPDATED STUFF

--- script.py
+++ script.py
@@ -1,12 +1,10 @@
-## This script made by Esmail EL BoB, Under GNU GPL V3
-
 import smtplib
 from email.mime.text import MIMEText
 from datetime import datetime, timedelta, date
 
 
 def add_day():
-    EndDate = date.today() + timedelta(days=1) #It will add one more day on today's date so feel free to change "days" if you want it add 2 days or more instead.
+    EndDate = date.today() + timedelta(days=1) # It will add one more day on today's date so feel free to change "days" if you want it add 2 days or more instead.
 
     fileName = "date.txt"
     accessMode = "w"
@@ -21,12 +19,12 @@ def add_day():
 def send_email():
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls()
-    s.login("Your_Email@gmail.com", "Your_Password") #Type your email and password here
+    s.login("YOUR_USERNAME@gmail.com", "YOUR_PASSWORD") # Type your email and password here to login
     s.set_debuglevel(1)
-    msg = MIMEText("""Hey, Test""") #Type your message here
-    sender = 'Your_Email@gmail.com' #Again, type your email here
-    recipients = ['john.doe@example.com', 'john.smith@example.co.uk'] #type people to send them the email here
-    msg['Subject'] = "I'm dead" #subject of the email
+    msg = MIMEText("""Hey, Test""") # Type your message here
+    sender = 'YOUR_USERNAME@gmail.com' # Again, type your email here
+    recipients = ['YOU_FRIEND_EMAIL@gmail.com', 'YOUR_FAMILY_EMAIL@hotmail.com', 'YOUR_TEACHER@yahoo.com'] # Type people to send them the email here
+    msg['Subject'] = "I'm dead" # Subject of the email
     msg['From'] = sender
     msg['To'] = ", ".join(recipients)
     s.sendmail(sender, recipients, msg.as_string())
@@ -49,28 +47,31 @@ def check_day():
         elif firstline != str(date.today()):
             send_email()
     except FileNotFoundError:
-        print("please create a file called 'date.txt'")
+        input("Please create a file called 'date.txt'\n\n(Press Enter To Leave!) ")
     
 
 def login():
     user_password = input("Please type your password: ") # Here is your pass-code. If it's right the file will be written in tomorrow's date
 
     if user_password.lower() == "password": #do not forget change this one too! 
-        print("glad to hear that you are alive!\ncome back tmr!")
-
+        input("Glad to hear that you are alive!\ncome back tmr!\n\n(Press Enter To Leave!) ")
         add_day()
 
     elif user_password.lower != "password": #and this!
-        for password in range(3):
-            user_password_again = input("Password is wrong, Please type your password: ")
-            if user_password_again.lower() == "password" :
-                add_day()
-                print("glad to hear that you are alive!\ncome back tmr!")
-                break
-            ("Seems you dead, RIP i'm going to send emails now")
+        def retry():
+            for password in range(3):
+                user_password_again = input("Password is wrong, Please type your password: ")
+                if user_password_again.lower() == "password" :
+                    return "Logged in"
+                    break
+
+        if retry() != "Logged in" :
+            print("\n\nSeems you dead, RIP i'm going to send emails now\n\n")
             send_email()
+            input("\n\nEmails, Sent!\n\n(Press Enter To Leave!) ")
+
             
 
-
+# Calling the function
 check_day()
 
