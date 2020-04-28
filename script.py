adee1d44c06659608dad7e3a8d0bc934e74e7082
Esmail EL BoB <esmailelbob@protonmail.com>
2020-04-28 11:42:13 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
 import smtplib
 import threading
 import os
+import time
 
-import cronus.beat as beat ## Install it `pip install cronus`, it's what make me repeat the notification code
 from email.mime.text import MIMEText
 from datetime import datetime, timedelta, date
 from notify_run import Notify ## Install it `pip install notify-run`, it's what make me send you the notifications
@@ -97,10 +97,9 @@ def send_email():
     s.starttls()
     s.login("YOUR_USERNAME@gmail.com", "YOUR_PASSWORD") # Type your email and password here to login
     s.set_debuglevel(1)
-    msg = MIMEText("""Hey, Test""") # Type your message here
-    sender = 'YOUR_USERNAME@gmail.com' # Again, type your email here
-    recipients = ['YOU_FRIEND_EMAIL@gmail.com', 'YOUR_FAMILY_EMAIL@hotmail.com', 'YOUR_TEACHER@yahoo.com'] # Type people to send them the email here
-    msg['Subject'] = "I'm dead" # Subject of the email
+    msg = MIMEText("""Hey, Test""") ##Type your message here
+    sender = 'YOUR_USERNAME@gmail.com' ##Again, type your email here
+    recipients = ['YOU_FRIEND_EMAIL@gmail.com', 'YOUR_FAMILY_EMAIL@hotmail.com', 'YOUR_TEACHER@yahoo.com'] # Type people to send them the email here    msg['Subject'] = "I'm dead" ##subject of the email
     msg['From'] = sender
     msg['To'] = ", ".join(recipients)
     s.sendmail(sender, recipients, msg.as_string())
@@ -111,19 +110,20 @@ def send_email():
 
 ## Enable it when you are on phone, due its useless on PC because script run on startup only but on phone it will be run most of time
 ## To make it works, install it inside your termux type `pip install notify-run` then type `notify-run register` and go to link and subscribe inside your browser and keep it open whole day (the borwser and the script)
-def notify_repeter():
-    beat.set_rate(2) # 2 Hz(Hz = Seconds)
-    while beat.true():
-        notify = Notify()
-        notify.send('Dead Man\'s Switch App\nDo not forget to Check Your Dead Man\'s switch app')
+# def notify_repeter():
+#     starttime = time.time()
+#     while True:
+#         notify = Notify()
+#         notify.send('Dead Man\'s Switch App\nDo not forget to Check Your Dead Man\'s switch app')
+#         time.sleep(21600.0 - ((time.time() - starttime) % 60.0)) # Time in seconds
 
 
-def notify_thereder():
-    notify_thread = threading.Thread(target=notify_repeter)
-    notify_thread.start()
+# def notify_thereder():
+#     notify_thread = threading.Thread(target=notify_repeter)
+#     notify_thread.start()
 
-## Start Notification System
-notify_thereder()
+# ## Start Notification System
+# notify_thereder()
 ## Start the app
 check_day()
 
