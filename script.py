bfb5ed7d89bafd3ff8d752bace63826303571e11
Esmail EL BoB <esmailelbob@protonmail.com>
2020-04-30 22:14:32 +0200

    I guess i'm done!
    I assume i'm done now.
    
    Added a new security "feature" to ensure no one changed date inside file and  made the code more nice looking and fixed some weirdo bugs

--- script.py
+++ script.py
@@ -1,63 +1,29 @@
-import smtplib, threading, os, time, getpass, keyring ## Install it `pip install keyring`
+import smtplib, threading, os, time, getpass, base64, keyring ## Install it `pip install keyring`, if you are using KWallet please see: https://github.com/jaraco/keyring#installation---linux
 from email.mime.text import MIMEText
 from datetime import datetime, timedelta, date
 ## Install it `pip install notify-run`, it's what make me send you the notifications
 # from notify_run import Notify 
 
 
-def user_password():
-    app_name = 'Dead Man Swtich APP'
-    user_name = getpass.getuser()
-
-    if keyring.get_password(app_name, user_name) == "None":
-        user_password_input = input("Please enter your password(first time only): ")
-        keyring.set_password(app_name, user_name, user_password_input)
-        user_password = keyring.get_password(app_name, user_name)
-        return user_password
-
-    elif keyring.get_password(app_name, user_name) != "None":
-        user_password = keyring.get_password(app_name, user_name)
-        return user_password    
-
-# Read Dates from files 
-def read_from_files(fileName, accessMode):
-    try:
-        with open(fileName, accessMode) as name:
-            file = name.readline()
-        return file
-    except FileNotFoundError:
-        input("Please create a file called 'tmr_date.txt' and 'today_date.txt'\n\n(Press Enter To Leave!)\n\n")
-    except:
-        input("\n\nOops, something went wrong\n\n(Press Enter To Leave!)\n\n")   
-
-# Write Dates to files
-def write_to_files(fileName, accessMode, data):
-    try:
-        file = open(fileName, accessMode)
-        file.write(data)
-    except:
-        input("\n\nOops, something went wrong while writing date to files\n\n(Press Enter To Leave!)\n\n")    
-    finally:
-        file.close()
-
-
-
 ##  Check for days, If date right and if not i will send emails  
 def check_day():
     today_file = read_from_files("today_date.txt", "r")
     tmr_file = read_from_files("tmr_date.txt", "r")
-
-    if today_file == str(date.today()) or tmr_file == str(date.today()):
+    tmr_file_hash = read_from_files("tmr_date_hash.txt", "r")
+            
+    if tmr_file == str(date.today()) and tmr_file_hash == str(encrypt_date(date.today())):
         login()
-    elif today_file == "" or tmr_file == "":
+    elif today_file == str(date.today()):
         login()
-    elif tmr_file != str(date.today()):
+    elif today_file == "" and tmr_file == "" and tmr_file_hash == "":
+        login()
+    else:
         send_email()
-    
+
 
 ## Checks password right
 def login():
-    user_password_input = input("Please type your password(or `exit` to leave): ") ## Here is your pass-code. If it's right the file will be written in tomorrow's date
+    user_password_input = input("Please type your password(or `exit` to leave or press ctrl + c): ") ## Here is your pass-code. If it's right the file will be written in tomorrow's date
 
     if user_password_input == user_password(): ##do not forget change this one too! 
         input("\n\nGlad to hear that you are alive!\ncome back tmr!\n\n(Press Enter To Leave!)\n\n")
@@ -91,19 +57,64 @@ def login():
             check_day()
 
 
+def user_password():
+    app_name = 'Dead Man Swtich APP'
+    user_name = getpass.getuser()
 
+    if keyring.get_password(app_name, user_name) == None:
+        user_password_input = input("I assume it's your first time here so please add your password(first time only): ")
+        keyring.set_password(app_name, user_name, user_password_input)
+        user_password = keyring.get_password(app_name, user_name)
+        return user_password
 
-
-
+    elif keyring.get_password(app_name, user_name) != None:
+        user_password = keyring.get_password(app_name, user_name)
+        return user_password    
 
 
 ## Adds day on today's date to check next time when you open
 def add_day():
-    EndDate = date.today() + timedelta(days=1) ##It will add one more day on today's date so feel free to change "days" if you want it add 2 days or more instead.
-
+    # decrypt = base64.decodebytes(today_date)
+    # decrypt2 = base64.decodebytes(end_date)
     today = write_to_files("today_date.txt", "w", str(date.today()))
-    tmr = write_to_files("today_date.txt", "w", str(EndDate))
-            
+
+    tmr_date_encrypt = str(encrypt_date(date.today() + timedelta(days=1)))
+    tmr_hash = write_to_files("tmr_date_hash.txt", "w", tmr_date_encrypt)
+    tmr = write_to_files("tmr_date.txt", "w", str(date.today() + timedelta(days=1)))
+
+
+## To ensure safty of saved dates
+def encrypt_date(data):
+    day = str(data)
+    day_date = day.encode('utf-8')
+    encrypt = base64.b64encode(day_date)
+    return encrypt.decode('utf-8')
+
+
+# Read Dates from files 
+def read_from_files(fileName, accessMode):
+    try:
+        with open(fileName, accessMode) as name:
+            file = name.readline()
+        return file
+    except FileNotFoundError:
+        input("Please create a file called 'tmr_date.txt', 'today_date.txt' and 'tmr_date_hash.txt'\n\n(Press Enter To Leave!)\n\n")
+    except:
+        input("\n\nOops, something went wrong\n\n(Press Enter To Leave!)\n\n")   
+
+
+# Write Dates to files
+def write_to_files(fileName, accessMode, data):
+    try:
+        file = open(fileName, accessMode)
+        file.write(data)
+    except:
+        input("\n\nOops, something went wrong while writing date to files\n\n(Press Enter To Leave!)\n\n")    
+    finally:
+        file.close()
+
+
+
 ## Sends emails to people you mentioned after you die
 def send_email():
     s = smtplib.SMTP('smtp.gmail.com', 587)
@@ -117,7 +128,6 @@ def send_email():
     msg['From'] = sender
     msg['To'] = ", ".join(recipients)
     s.sendmail(sender, recipients, msg.as_string())
-
 
 
 
