d781599db6bc28af6ad8aa08f56969fa9482428f
Esmail EL BoB <esmailelbob@protonmail.com>
2020-05-10 03:33:23 +0200

    you can now send HTML emails :+1:
    so i improved the codes (kinda) and gave example of how to send HTML emails if you want send photos or memes in your death email as what i did, lol

--- script.py
+++ script.py
@@ -5,8 +5,10 @@ from datetime import datetime, timedelta, date
 # from notify_run import Notify 
 
 
-##  Check for days, If date right and if not i will send emails  
 def check_day():
+    """Check for days, If date right and if not it will send emails.
+    This will work like you open the app so it will check if it's today's date it will say you come tomorrow and if it's tomorrow's date it will ask you for password to add one more day and everything will be good but if its not today's or tomorrow's date it will send emails right away!"""
+
     ## Check if list "out of range" or in other words that file is empty it will set values to nothing to make you login
     try:
         today_date = read_from_files("data.txt", "r")[1]
@@ -17,29 +19,37 @@ def check_day():
         tomorrow_date = []
         tomorrow_date_hash = []
 
+   ## Check for the date as i said above!
     if today_date == str(date.today()) + "\n":
         input("\n\nYou already checked today, Please come tomorrow!\n\n(Press Enter To Leave!)\n\n")
         exit()
+    
     elif tomorrow_date == str(date.today()) + "\n" and tomorrow_date_hash == str(encrypt_date(date.today())):
         login()
+    
     elif today_date == [] and tomorrow_date == [] and tomorrow_date_hash == []:
         login()
+    
     else:
         send_email()
         exit()
 
 
-## Checks password right
 def login():
+    """This Function check for password and password saved in your keyring so do not worry!"""
+
     user_password_input = getpass.getpass("Please type your password(or `exit` to leave or press ctrl + c): ")
- ## Here is your pass-code. If it's right you will be able to login to add tomorrow's date
+ ## Here is your password. If it's right you will be able to login to add tomorrow's date
 
     if user_password_input == user_password():
         add_day()
+    
     elif user_password_input.lower() == "exit":
         kill = 'pkill -f ' + os.path.basename(__file__) ## `os.path.basename(__file__)` code gets current name of this script so no need to update the name here everytime you change the script's name
         os.system(kill)
+    
     elif user_password_input != user_password():
+        ## I know code messy but this code give you more 3 chances to add right password
         def retry_password():
             for password_again in range(3):
                 user_password_again = getpass.getpass("Password is wrong, please try again (it's case sensitive): ")
@@ -58,6 +68,8 @@ def login():
 
 
 def user_password():
+    """This function to add your password in keyring incase if its not"""
+
     app_name = 'Dead Man Swtich APP'
     user_name = getpass.getuser() ##Gets your username, you not need it actually it just to know for which user password is set 
 
@@ -73,10 +85,11 @@ def user_password():
 
 
 
-## Adds day on today's date to check next time when you open
 def add_day():
-    
+    """This function add the tomorrow's date into the data file"""
+
     data = write_to_files("data.txt", "w", "Today Date: " + "\n" + str(date.today()) + "\n\n" + "Tomorrow Date: " + "\n" + str(date.today() + timedelta(days=1)) + "\n\n" + "Tomorrow Hash: " + "\n" + str(encrypt_date(date.today() + timedelta(days=1))))
+
     input("\n\nGlad to hear that you are alive!\ncome back tomorrow!\n\n(Press Enter To Leave!)\n\n")
 
     ## Use this code if you want to "decrypt" the base64, for me i not need it --until now--
@@ -84,16 +97,18 @@ def add_day():
     # decrypt2 = base64.decodebytes(end_date)
 
 
-## To ensure safty of saved dates
 def encrypt_date(data):
+    """This function converts the tomorrow's date into base64 as kind of hash to ensure no one played in the dates"""
+
     day = str(data)
     day_date = day.encode('utf-8')
     encrypt = base64.b64encode(day_date)
     return encrypt.decode('utf-8')
 
 
-# Read Dates from files 
 def read_from_files(fileName, accessMode):
+    """This function read from data file to get data and work"""
+
     try:
         with open(fileName, accessMode) as name:
             file = name.readlines()
@@ -106,6 +121,8 @@ def read_from_files(fileName, accessMode):
 
 # Write Dates to files
 def write_to_files(fileName, accessMode, data):
+    """This function write to files the data"""
+
     try:
         file = open(fileName, accessMode)
         file.write(data)
@@ -116,15 +133,33 @@ def write_to_files(fileName, accessMode, data):
 
 
 
-## Sends emails to people you mentioned after you die
 def send_email():
+    """This function send emails to people"""
+
     print("\n\nSeems you dead, RIP i'm going to send emails now\n\n")
 
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls()
     s.login("YOUR EMAIL@gmail.com", "YOUR PASSWORD") ##Type your email and password here
     s.set_debuglevel(1)
-    msg = MIMEText("""BODY OF THE EMAIL (The message)""") ##Type your message here
+    msg = MIMEText("""BODY OF THE EMAIL (The message)""", "plain") ##Type your message here and Change "plain" to "html" (with quotes) if you want to type HTML email instead!
+
+
+    ## An example of HTML email
+
+    # msg = MIMEText("""\
+    #     <html>
+    #     <head></head>
+    #     <body>
+    #         <p>Hi!<br>
+    #         How are you?<br>
+    #         Here is the <a href="http://www.python.org">link</a> you wanted.
+    #         </p>
+    #     </body>
+    #     </html>
+    #     """, 'html')
+
+
     sender = 'YOUR EMAIL@gmail.com' ##Again, type your email here
     recipients = ['YOUR Friend@gmail.com', 'Your Family@hotmail.com', 'Your Teacher@yahoo.com'] ##type people to send them the email here
     msg['Subject'] = "SUBJECT OF THE EMAIL (The Title/Name)" ##subject of the email
@@ -140,7 +175,10 @@ def send_email():
 
 ## Enable it when you are on phone, due its useless on PC because script run on startup only but on phone it will be run most of time
 ## To make it works, install it inside your termux type `pip install notify-run` then type `notify-run register` and go to link and subscribe inside your browser and keep it open whole day (the borwser and the script)
+
 # def notify_repeter():
+#     """This function keep repeating and send notifications"""
+
 #     starttime = time.time()
 #     while True:
 #         notify = Notify()
@@ -149,11 +187,15 @@ def send_email():
 
 
 # def notify_thereder():
+#     """This is the thread function to keep notifications work in background"""
+
 #     notify_thread = threading.Thread(target=notify_repeter)
 #     notify_thread.start()
 
+
 ## Start Notifications System
 # notify_thereder()
+
 ## Start the app
 check_day()
 
